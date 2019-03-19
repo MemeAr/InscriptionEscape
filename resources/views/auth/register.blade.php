ce723694d713c87a41f96734ac469253f9674c1c
Amelie <amelie.ar98@gmail.com>
2019-03-19 17:44:16 +0100

    Le test qui va tout determiner

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header text-white bg-dark">{{ __('Inscription Escape Game TSE') }}</div>
+                <div class="card-header text-white bg-dark">{{ __('Inscription Mémé') }}</div>
 
                 <div class="card-body text-white bg-dark">
                     <form method="POST" action="{{ route('register') }}">
