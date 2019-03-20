a227c5014be6301b758cd826839279e57a65ab61
Unknown <elise.andaloro@hotmail.fr>
2019-03-20 17:00:03 +0100

    Route fonctionnelles pour UsersController + message

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/message', function(){
+    return view('/message');
+});
+
+Route::get('/users', 'UsersController@getAllUsers');
+
+Route::get('/triusers', 'UsersController@triUsers');
